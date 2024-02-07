57f44eb013102b00d1049496e54cc1e835781b4c
Matt Iselin <matthew@repl.it>
2024-02-07 11:46:15 -0800

    repro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,17 @@
-/** Add your relevant code here for the issue to reproduce */
+
+const MOBILE_MAX = 767;
+
 export default function Home() {
-  return null;
+  return (
+    <div>
+      <h1 className="header">Hello</h1>
+      <style jsx>{`
+        .header { font-size: 48px; }
+
+        @media screen and (max-width: ${MOBILE_MAX}px) {
+          .header { font-size: 12px; }
+        }
+      `}</style>
+    </div>
+  );
 }
